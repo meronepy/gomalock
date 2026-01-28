6941c356c126f0beb28c6be0d3f14eeacb34b109
meronepy <203420124+meronepy@users.noreply.github.com>
2026-01-28 11:16:13 +0900

    refactor: add more explanation in sample script

--- examples/setup_new_sesame.py
+++ examples/setup_new_sesame.py
@@ -11,9 +11,11 @@ AUTOLOCK_DURATION = 5
 async def main():
     async with Sesame5(MAC_ADDRESS) as sesame5:
         secret_key = await sesame5.register()
+        print(secret_key)
+
+        # Configure the lock with desired settings (optional)
         await sesame5.set_lock_position(LOCK_POSITION, UNLOCK_POSITION)
         await sesame5.set_auto_lock_duration(AUTOLOCK_DURATION)
-        print(secret_key)
 
 
 if __name__ == "__main__":
