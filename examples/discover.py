34898e86bcbb6f110841f52ff36ee8ac637d37b4
meronepy <adsumchi@outlook.com>
2026-06-04 21:52:59 +0900

    fix: make the example compatible with the new API

--- examples/discover.py
+++ examples/discover.py
@@ -13,11 +13,15 @@ async def main():
     """Scan for Sesame BLE devices and print their information."""
     print("-" * 50)
     devices = await gomalock.SesameScanner.discover(timeout=30)
-    for address, sesame_adv_data in devices.items():
-        print(f"{'Address':11}: {address}")
-        print(f"{'Model':11}: {sesame_adv_data.product_model.name}")
-        print(f"{'Registered':11}: {sesame_adv_data.is_registered}")
-        print(f"{'UUID':11}: {sesame_adv_data.device_uuid}")
+    for scanned_device in devices.values():
+        print(f"{'Address':11}: {scanned_device.mac_address}")
+        print(
+            f"{'Model':11}: {scanned_device.sesame_advertisement_data.product_model.name}"
+        )
+        print(
+            f"{'Registered':11}: {scanned_device.sesame_advertisement_data.is_registered}"
+        )
+        print(f"{'UUID':11}: {scanned_device.sesame_advertisement_data.device_uuid}")
         print("-" * 50)
 
 
