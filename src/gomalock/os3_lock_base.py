9fcc02f36583b1302fadddaf97e90deb6cde6386
meronepy <adsumchi@outlook.com>
2026-06-06 15:06:32 +0900

    Revert "fix: fix pylint reportGeneralTypeIssues error"
    This reverts commit d7facec10ac5d38b6fc5067728eed9b145c6150f.

--- src/gomalock/os3_lock_base.py
+++ src/gomalock/os3_lock_base.py
@@ -30,7 +30,7 @@ logger = logging.getLogger(__name__)
 
 
 # Holds device state, so pylint: disable=too-many-instance-attributes
-class BaseSesameOS3Lock[LockSelfT, MechStatusT](ABC):
+class BaseSesameOS3Lock[LockSelfT: "BaseSesameOS3Lock", MechStatusT](ABC):
     """Abstract base class for interacting with Sesame OS3 devices.
 
     Provides common functionality such as connecting, logging in, handling
